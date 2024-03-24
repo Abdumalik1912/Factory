80cc9eeeac8a5a58cae5e4520b750b1c5ea6037d
Abdumalik Abdusalomov <aabdusalomov533@gmail.com>
2024-03-24 16:22:37 +0500

    fifth commit

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'account',
+    'account.apps.AccountConfig',
     'products.apps.ProductsConfig',
 ]
 
